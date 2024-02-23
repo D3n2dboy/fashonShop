2167a88ba94178e99c9daf18c6bf226b8b302f4e
Ryazanov Denis <d3n2dboy@gmail.com>
2024-02-23 19:13:19 +0500

    init slider swiper no adaptiv version

--- src/js/app.js
+++ src/js/app.js
@@ -14,56 +14,56 @@ d3nFunctions.menuInit();
 //import { Navigation, Pagination } from 'swiper/modules';
 //import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
-//import Swiper from 'swiper';
-//import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 // import Swiper and modules styles
-//import 'swiper/css';
+import 'swiper/css';
 // Стили Swiper
 // Базовые стили
-//import "../../scss/base/swiper.scss";
+
 // Полный набор стилей из scss/libs/swiper.scss
-//import "../../scss/libs/swiper.scss";
+// import "../../scss/libs/swiper.scss";
 // Полный набор стилей из node_modules
 
 
-// // import styles bundle
-// function initSliders() {
-//     // Перечень слайдеров
-//     // Слайдер главной страницы new
-//     if (document.querySelector('.swiper')) { // Указываем скласс нужного слайдера
-//         // Создаем слайдер
-//         new Swiper('.swiper', { // Указываем скласс нужного слайдера
-//             // Подключаем модули слайдера
-//             // для конкретного случая
-//             modules: [Navigation, Pagination],
-//             autoHeight: false,
-//             speed: 600,
+// import styles bundle
+function initSliders() {
+    // Перечень слайдеров
+    // Слайдер главной страницы new
+    if (document.querySelector('.swiper')) { // Указываем скласс нужного слайдера
+        // Создаем слайдер
+        new Swiper('.swiper', { // Указываем скласс нужного слайдера
+            // Подключаем модули слайдера
+            // для конкретного случая
+            modules: [Navigation, Pagination],
+            autoHeight: false,
+            speed: 600,
 
-//             autoplay: {
-//                 delay: 4000,
-//                 pauseOnMouseEnter: true,
-//                 disableOnInteraction: true,
-//             },
+            autoplay: {
+                delay: 4000,
+                pauseOnMouseEnter: true,
+                disableOnInteraction: true,
+            },
 
-//             //Пагинация
-//             pagination: {
-//                 el: '.perview-swiper-pagination',
-//                 clickable: true,
-//             },
+            //Пагинация
+            pagination: {
+                el: '.perview-swiper-pagination',
+                clickable: true,
+            },
 
-//             // Кнопки "влево/вправо"
-//             navigation: {
-//                 prevEl: '.perview-swiper-button-prev',
-//                 nextEl: '.perview-swiper-button-next',
-//             },
+            // Кнопки "влево/вправо"
+            navigation: {
+                prevEl: '.perview-swiper-button-prev',
+                nextEl: '.perview-swiper-button-next',
+            },
 
-//         });
-//     }
-// }
+        });
+    }
+}
 
-// window.addEventListener("load", function (e) {
-//     // Запуск инициализации слайдеров
-//     initSliders();
-//     // Запуск инициализации скролла на базе слайдера (по классу swiper_scroll)
-//     //initSlidersScroll();
-// });
+window.addEventListener("load", function (e) {
+    // Запуск инициализации слайдеров
+    initSliders();
+    // Запуск инициализации скролла на базе слайдера (по классу swiper_scroll)
+    //initSlidersScroll();
+});
